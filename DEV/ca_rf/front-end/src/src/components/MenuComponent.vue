e1a1b82ca3d89d38ec70e7693b30844edbd8457c
dev-mobilla <101626428+Dev-Mobilla@users.noreply.github.com>
2022-08-01 14:48:46 +0800

    front end modify
    cash advance & revolving fund alert modify

---
+++ DEV/ca_rf/front-end/src/src/components/MenuComponent.vue
@@ -0,0 +1,39 @@
+<template>
+    <v-container class="m-auto d-flex"
+        style="background-color: #0000008f;border-radius: 13px; max-width: 65%;margin-top: -150px; height: 60vh;">
+        <!-- <v-card max-width="800" height="400" color="#61616" style="background-color: #0000008f;border-radius: 15px;" class="mx-auto"> -->
+        <v-row justify="space-around" align="center" style="margin: 0 auto;">
+            <v-card width="40%" height="65%" to="cash-advance"
+                style="cursor: pointer;display: flex;align-items:center;justify-content: center;flex-direction: column;">
+                <div>
+                    <v-img src="../assets/cash-advance.webp" max-width="130"></v-img>
+                </div>
+                <p style="font-size: 35px;font-weight: 600;">
+                    CASH ADVANCE
+                </p>
+            </v-card>
+            <v-card width="40%" height="65%" to="revolving-fund"
+                style="cursor: pointer;display: flex;align-items:center;justify-content: center;flex-direction: column;">
+                <div>
+                    <v-img src="../assets/revolving-fund.webp" max-width="130"></v-img>
+                </div>
+                <p style="font-size: 35px;font-weight: 600;">
+                    REVOLVING FUND
+                </p>
+            </v-card>
+        </v-row>
+        <!-- </v-card> -->
+    </v-container>
+</template>
+<script>
+export default {
+    name:'MenuComponent',
+    methods:{
+        
+    }
+
+}
+</script>
+<style>
+
+</style>
